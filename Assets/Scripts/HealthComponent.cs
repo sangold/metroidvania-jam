845f2ac9411770b0f230f581c619b608c7505aee
sangold <smcx01@gmail.com>
2021-11-23 13:46:33 +0100

    HealthComponent handles stun immunity

--- Assets/Scripts/HealthComponent.cs
+++ Assets/Scripts/HealthComponent.cs
@@ -4,31 +4,42 @@ using UnityEngine;
 
 public class HealthComponent : MonoBehaviour
 {
-    [SerializeField] private int _health;
+    [SerializeField] private int _currentHealth;
+    [SerializeField] private int _maxHealth;
     // Might change into an Actioif we move rendering to it's own component
     [SerializeField] private SpriteRenderer _sr;
     private Color _originalColor;
     private float _flashDuration = .08f;
+    private bool _isStunned;
+
+    public event Action<int> OnDamageTaken;
+    public float StunDuration;
+
+    public int Health { get => _currentHealth; set => _currentHealth = Mathf.Clamp(value, 0, _maxHealth); }
 
     private void Awake()
     {
         _originalColor = _sr.color;
+        _currentHealth = _maxHealth;
     }
 
     public void TakeDamage(int amount)
     {
-        _health -= amount;
+        if (_isStunned) return;
+        _currentHealth -= amount;
         StopCoroutine(Blink(0f));
-        StartCoroutine(Blink(.75f));
-
-        if(_health <= 0)
+        StartCoroutine(Blink(StunDuration));
+        if(_currentHealth <= 0)
         {
             Kill();
+            return;
         }
+        OnDamageTaken?.Invoke(_currentHealth);
     }
 
     private IEnumerator Blink(float duration)
     {
+        _isStunned = true;
         float timer = 0;
         while(timer < duration)
         {
@@ -38,6 +49,7 @@ public class HealthComponent : MonoBehaviour
             yield return new WaitForSeconds(_flashDuration);
             timer += _flashDuration * 2;
         }
+        _isStunned = false;
     }
 
     private void Kill()
