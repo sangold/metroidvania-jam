7905aa6cbec8fe6f12a19cc194d47951cb5e9211
Sandy <sangold@users.noreply.github.com>
2021-12-03 18:49:32 +0100

    Adding the UI for pause and map

---
+++ Assets/Scripts/UI/UILevelSquare.cs
@@ -0,0 +1,51 @@
+using UnityEngine;
+using UnityEngine.UI;
+
+public class UILevelSquare : MonoBehaviour
+{
+    [SerializeField] private Image _image;
+    [SerializeField] private Outline _outline;
+    [SerializeField] private RectTransform _rt;
+    private float _squareSize = 30f;
+    private Color _darkColor;
+    private Color _mainColor;
+    private Color _lightColor;
+    private Color _lighterColor;
+    private bool _isExplored = false;
+    private bool _isActive = false;
+
+    public void Init(Level level, Transform parent, Vector2 offset)
+    {
+        SetColor(
+            Level.GetDarkestColor(level.World),
+            Level.GetMainColor(level.World),
+            Level.GetLightColor(level.World),
+            Level.GetLightestColor(level.World)
+        );
+        transform.SetParent(parent);
+        _rt.anchoredPosition = new Vector3(
+            level.PosX * _squareSize + offset.x,
+            -(level.PosY * _squareSize) - offset.y,
+            0
+        );
+    }
+
+    public void SetColor(Color d, Color m, Color l, Color lr)
+    {
+        _darkColor = d;
+        _mainColor = m;
+        _lightColor = l;
+        _lighterColor = lr;
+    }
+    public void SetActive(bool isActive)
+    {
+        _isActive = isActive;
+        _outline.effectColor = isActive ? _lighterColor : _darkColor;
+    }
+
+    public void SetExplored(bool isExplored)
+    {
+        _isExplored = isExplored;
+        _image.color = isExplored ? _mainColor : _lightColor;
+    }
+}
---
+++ Assets/Scripts/UI/UIMapView.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class UIMapView: MonoBehaviour
+{
+    private List<Level> _levels;
+    private Dictionary<Level, GameObject> _levelsGOs;
+    [SerializeField] private GameObject _levelSquareGO;
+    [SerializeField] private float horizontalMargin = 0f;
+    [SerializeField] private float verticalMargin = 0f;
+
+
+    private void Awake()
+    {
+        _levelsGOs = new Dictionary<Level, GameObject>();
+    }
+
+    public void Open()
+    {
+        gameObject.SetActive(true);
+        InitGrid();
+    }
+    public void Close()
+    {
+        gameObject.SetActive(false);
+    }
+
+    private void InitGrid()
+    {
+        
+        _levels = GameManager.Instance.Levels;
+
+        Debug.Log(_levels.Count);
+        foreach(var level in _levels)
+        {
+            if (!level.IsVisible) continue;
+            if(!_levelsGOs.ContainsKey(level))
+            {
+                var levelGO = Instantiate(_levelSquareGO);
+                levelGO.GetComponent<UILevelSquare>().Init(level, transform, new Vector2(horizontalMargin, verticalMargin));
+                _levelsGOs.Add(level, levelGO);
+            }
+            var uiSquare = _levelsGOs[level].GetComponent<UILevelSquare>();
+            uiSquare.SetActive(level.IsActive);
+            uiSquare.SetExplored(level.IsExplored);
+        }
+    }
+}
+
