a3f334a9217e1c20b57976564d0983e0d7265e4f
Sandy <sangold@users.noreply.github.com>
2021-12-03 10:40:43 +0100

    Create a dumber StateMachine

---
+++ Assets/Scripts/StateMachine/StateMachine.cs
@@ -0,0 +1,22 @@
+﻿public class StateMachine
+{
+   
+    protected IState _currentState;
+
+
+    public virtual void Tick()
+    {
+        _currentState?.Tick();
+    }
+
+    public virtual void SetState(IState toState)
+    {
+        if (toState == _currentState)
+            return;
+
+        _currentState?.OnExit();
+        _currentState = toState;
+
+        _currentState?.OnEnter();
+    }
+}
