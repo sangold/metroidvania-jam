5f5d545a40181dff65667248d9e9092f0bef8f44
Sandy <sangold@users.noreply.github.com>
2021-12-10 22:37:13 +0100

    ScriptableEvent

---
+++ Assets/Scripts/Events/ModalContentSO.cs
@@ -0,0 +1,9 @@
+﻿using UnityEngine;
+
+public class ModalContentSO: ScriptableObject
+{
+    public string Title;
+    [TextArea(10, 30)]
+    public string Content;
+    public Sprite Image;
+}
---
+++ Assets/Scripts/Events/UIModalEvent.cs
@@ -0,0 +1,13 @@
+﻿using UnityEngine;
+using UnityEngine.Events;
+
+[CreateAssetMenu(menuName = "Events/UIModalEvent", fileName = "New modal event")]
+public class UIModalEvent: ScriptableObject
+{
+    public UnityAction<ModalContentSO> OnModalOpen;
+
+    public void RaiseEvent(ModalContentSO modalContent)
+    {
+        OnModalOpen?.Invoke(modalContent);
+    }
+}
