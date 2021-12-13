93354be0efe9a3be03ac1e3a5544de7137287a36
Sandy <sangold@users.noreply.github.com>
2021-12-13 19:20:05 +0100

    Start New Gamedata

---
+++ Assets/Scripts/GameDataManager/GameData.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections.Generic;
+using UnityEngine;
+
+[System.Serializable]
+public class GameData
+{
+    // Player Data
+    public Vector3 playerPosition; // Last visited portal exit point
+    public int CurrentLevelIndex;
+    public PlayerStatsSO PlayerStats;
+
+    public List<Level> Levels;
+
+}
--- Assets/Scripts/GameDataManager/GameDataManager.cs
+++ Assets/Scripts/GameDataManager/GameDataManager.cs
@@ -4,7 +4,7 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public static class GameDataManager
-{
+{    
     public static bool hasDebugButtons = true;
     public static void saveData(){
         Player player = GameObject.FindWithTag("Player").GetComponent<Player>();
